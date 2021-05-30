45f0b923f144be59b0de325acec13e1e3c641970
Cameron Nepe <cameron.nepe@gmail.com>
2021-05-30 19:41:54 +1200

    Working with string functions

--- 04_strings.php
+++ 04_strings.php
@@ -1,10 +1,40 @@
 <?php
 
 // Create simple string
+$name = 'Cameron';
+$string1 = 'Hello my name is $name Nepe';
+$string2 = "Hello my name is $name Nepe"; // double quotes to concatenate
+echo $string1.'<br>';
+echo $string2.'<br>';
 
 // String concatenation
+echo 'Hello '.' World'.' and PHP'. '<br>';
 
 // String functions
+$string = "   Hello World   ";
+
+echo "1 - " . strlen($string) . '<br>' . PHP_EOL; 
+echo "2 - " . trim($string) . '<br>' . PHP_EOL;
+echo "3 - " . ltrim($string) . '<br>' . PHP_EOL;
+echo "4 - " . rtrim($string) . '<br>' . PHP_EOL;
+echo "5 - " . str_word_count($string) . '<br>' . PHP_EOL;
+echo "6 - " . strrev($string) . '<br>' . PHP_EOL;
+echo "7 - " . strtoupper($string) . '<br>' . PHP_EOL;
+echo "8 - " . strtolower($string) . '<br>' . PHP_EOL;
+echo "9 - " . ucfirst('hello') . '<br>' . PHP_EOL;
+echo "10 - " . lcfirst('HELLO') . '<br>' . PHP_EOL;
+echo "11 - " . ucwords('hello world') . '<br>' . PHP_EOL;
+echo "12 - " . strpos($string, 'world') . '<br>' . PHP_EOL; // Change into world
+echo "13 - " . stripos($string, 'world') . '<br>' . PHP_EOL;
+echo "14 - " . substr($string, 8) . '<br>' . PHP_EOL;
+echo "15 - " . str_replace('World', 'PHP', $string) . '<br>' . PHP_EOL;
+echo "16 - " . str_ireplace('world', 'PHP', $string) . '<br>' . PHP_EOL;
+
+$invoiceNumber = 100;
+$invoiceNumber2 = 123456;
+echo str_pad($invoiceNumber, 8, '0', STR_PAD_LEFT) . '<br>' . PHP_EOL;
+echo str_pad($invoiceNumber2, 8, '0', STR_PAD_LEFT) . '<br>' . PHP_EOL;
+echo str_repeat('Hello', 2) . '<br>' . PHP_EOL;
 
 // Multiline text and line breaks
 
